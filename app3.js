d1acc27a1de6642f6a69ed55cf493b2a9b2f6cdb
ricyboi <inoknowwhere@gmail.com>
2020-08-30 23:59:25 -0700

    Update app3.js

---
+++ app3.js
@@ -0,0 +1,12 @@
+function menuButton(x) {
+    x.classList.toggle("change");
+}
+function toggleNav() {
+    var x = document.getElementById("menuNav");
+    if (x.style.height === "100%") {
+    x.style.height = "0%";
+    } 
+    else {
+    x.style.height = "100%";
+    }
+}
