2ea54f8cbb114d27a51798a06728a20a7bd24bdb
jerrydengz <jerrydeng67@gmail.com>
2021-05-03 21:52:05 -0700

    added images of updated projects

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ function menuButton(x) {
 function toggleNav() {
     var x = document.getElementById("menuNav");
     if (x.style.height === "100%") {
-    x.style.height = "0%";
-    } 
-    else {
-    x.style.height = "100%";
+        x.style.height = "0%";
+    } else {
+        x.style.height = "100%";
     }
 }
